c82b2fdbed16a1f2903c3e5e8c9c13e0e2283f22
sankari k priya <87857167+SankariKolanchi@users.noreply.github.com>
2022-08-16 18:48:44 +0530

    Add files via upload

---
+++ home.dart
@@ -0,0 +1,235 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mentor_quiz_app_tut/answer.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<Icon> _scoreTracker = [];
+  int _questionIndex = 0;
+  int _totalScore = 0;
+  bool answerWasSelected = false;
+  bool endOfQuiz = false;
+  bool correctAnswerSelected = false;
+
+  void _questionAnswered(bool answerScore) {
+    setState(() {
+      // answer was selected
+      answerWasSelected = true;
+      // check if answer was correct
+      if (answerScore) {
+        _totalScore++;
+        correctAnswerSelected = true;
+      }
+      // adding to the score tracker on top
+      _scoreTracker.add(
+        answerScore
+            ? Icon(
+                Icons.check_circle,
+                color: Colors.green,
+              )
+            : Icon(
+                Icons.clear,
+                color: Colors.red,
+              ),
+      );
+      //when the quiz ends
+      if (_questionIndex + 1 == _questions.length) {
+        endOfQuiz = true;
+      }
+    });
+  }
+
+  void _nextQuestion() {
+    setState(() {
+      _questionIndex++;
+      answerWasSelected = false;
+      correctAnswerSelected = false;
+    });
+    // what happens at the end of the quiz
+    if (_questionIndex >= _questions.length) {
+      _resetQuiz();
+    }
+  }
+
+  void _resetQuiz() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+      _scoreTracker = [];
+      endOfQuiz = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'short gun',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                if (_scoreTracker.length == 0)
+                  SizedBox(
+                    height: 25.0,
+                  ),
+                if (_scoreTracker.length > 0) ..._scoreTracker
+              ],
+            ),
+            Container(
+              width: double.infinity,
+              height: 130.0,
+              margin: EdgeInsets.only(bottom: 10.0, left: 30.0, right: 30.0),
+              padding: EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+              decoration: BoxDecoration(
+                color: Colors.deepOrange,
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              child: Center(
+                child: Text(
+                  _questions[_questionIndex]['question'],
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            ...(_questions[_questionIndex]['answers']
+                    as List<Map<String, Object>>)
+                .map(
+              (answer) => Answer(
+                answerText: answer['answerText'],
+                answerColor: answerWasSelected
+                    ? answer['score']
+                        ? Colors.green
+                        : Colors.red
+                    : null,
+                answerTap: () {
+                  // if answer was already selected then nothing happens onTap
+                  if (answerWasSelected) {
+                    return;
+                  }
+                  //answer is being selected
+                  _questionAnswered(answer['score']);
+                },
+              ),
+            ),
+            SizedBox(height: 20.0),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: Size(double.infinity, 40.0),
+              ),
+              onPressed: () {
+                if (!answerWasSelected) {
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                    content: Text(
+                        'Please select an answer before going to the next question'),
+                  ));
+                  return;
+                }
+                _nextQuestion();
+              },
+              child: Text(endOfQuiz ? 'Restart Quiz' : 'Next Question'),
+            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                '${_totalScore.toString()}/${_questions.length}',
+                style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+            if (answerWasSelected && !endOfQuiz)
+              Container(
+                height: 100,
+                width: double.infinity,
+                color: correctAnswerSelected ? Colors.green : Colors.red,
+                child: Center(
+                  child: Text(
+                    correctAnswerSelected
+                        ? 'Well done, you got it right!'
+                        : 'Wrong :/',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            if (endOfQuiz)
+              Container(
+                height: 100,
+                width: double.infinity,
+                color: Colors.black,
+                child: Center(
+                  child: Text(
+                    _totalScore > 4
+                        ? 'Congratulations! Your final score is: $_totalScore'
+                        : 'Your final score is: $_totalScore. Better luck next time!',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                      color: _totalScore > 4 ? Colors.green : Colors.red,
+                    ),
+                  ),
+                ),
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+final _questions = const [
+  {
+    'question':' Q1. Who created Flutter?',
+    'answers': [
+      {'answerText': 'Facebook', 'score':  false},
+      {'answerText':  'Adobe', 'score': false},
+      {'answerText': 'Google', 'score':  true},
+    ],
+  },
+
+  {
+    'question':'Q2. What is Flutter?',
+    'answers': [
+      {'answerText':  'Android Development Kit', 'score': false},
+      {'answerText': 'IOS Development Kit', 'score':  false},
+      {'answerText': 'SDK to build beautiful IOS, Android, Web & Desktop Native Apps',
+        'score':   true},
+    ],
+  },
+
+  {
+    'question':'Q4. Who created Dart programing language?',
+    'answers': [
+      {'answerText':'Lars Bak and Kasper Lund', 'score':  false},
+      {'answerText': 'Brendan Eich', 'score':false},
+      {'answerText': 'Bjarne Stroustrup', 'score':  true},
+    ],
+  },
+
+  {
+    'question':' Q3. Which programing language is used by Flutter',
+    'answers': [
+      {'answerText':  'Ruby', 'score': false},
+      {'answerText': 'Dart', 'score':   false},
+      {'answerText':' Kotlin', 'score':
+         true},
+    ],
+  },
+
+];
